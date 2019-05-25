2e496a0d720f5d6e812d44e61d934d2f9c682b8e
writeens <44687555+writeens@users.noreply.github.com>
2019-05-25 18:26:51 +0100

    Create Split.js

---
+++ src/Split.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import "./Split.css";
+
+class Split extends Component {
+    render(){
+        let companyCom = 0.25 * this.props.fare;
+        let driverCom = this.props.fare - companyCom;
+        let split = (this.props.onTrip) ?
+            <div className="loader">
+                <p className="loading"></p>
+            </div> :
+                <div className="Split">
+                    <div className="Split-cost">Fare Cost: {(this.props.fare) ? `NGN ${this.props.fare}` : 0}</div>
+                    <div className="Split-details">
+                        <div>
+                            <p>Company</p>
+                            {(companyCom) ? <p className="Split-fee">{`NGN ${companyCom}`}</p> : ""}
+                        </div>
+                        <div>
+                            <p>Driver</p>
+                            {(driverCom) ? <p className="Split-fee">{`NGN ${driverCom}`}</p> : ""}
+                        </div>
+                    </div>
+                </div>
+        return(
+            <div className="Split-header">
+                {split}
+            </div>
+        )
+    }
+}
+
+export default Split;
